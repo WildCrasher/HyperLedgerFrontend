bf9d86dd18559cc92eb0b816f74c4674e8a6ef94
wildcrasher <dzikigeneral1996@o2.pl>
2020-09-27 21:16:55 +0200

    Connected with API.

--- src/app/shared/services/thesis.service.ts
+++ src/app/shared/services/thesis.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from './../../../environments/environment';
 import { ThesisDto } from './../dtos/thesis.dto';
@@ -14,12 +14,19 @@ export class ThesisService {
         this.url = environment.backendUrl;
     }
 
+    // createAuthorizationHeader(headers: HttpHeaders) {
+    //     headers.append('Content-Type', 'application/json');
+    //     headers.append('Access-Control-Allow-Origin', 'true')
+    // }
+
     addThesis(thesisData: any) {
         return this.http.post<ThesisDto>(this.url + 'thesis', thesisData);
     }
 
     getTheses() {
-        return this.http.get<ThesisDto[]>(this.url + 'thesis');
+        // let headers = new HttpHeaders();
+        // this.createAuthorizationHeader(headers);
+        return this.http.get<ThesisDto[]>(this.url + 'thesis'/*, {headers: headers}*/);
     }
 
     getThesis(id: string) {
--- src/app/theses-list/theses-list.component.ts
+++ src/app/theses-list/theses-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ThesisDto } from '../shared/dtos/thesis.dto';
 import { ThesisService } from '../shared/services/thesis.service';
 
 @Component({
@@ -9,32 +10,33 @@ import { ThesisService } from '../shared/services/thesis.service';
 })
 export class ThesesListComponent implements OnInit {
 
-    theses = [
-        {
-            'thesisNumber': 1,
-            'topic': "aaaa  vvv vvv",
-            'issueDateTime': '111111',
-            'state': 'free',
-            'student': 'brak',
-            'supervisor': 'promotor 1'
-        },
-        {
-            'thesisNumber': 2,
-            'topic': "aaaa  vvv vvv222222sssssssssssssssssssssssssssssssssss",
-            'issueDateTime': '111111',
-            'state': 'free',
-            'student': 'brak',
-            'supervisor': 'promotor 2'
-        },
-        {
-            'thesisNumber': 3,
-            'topic': "aaaa  vvv vvv3333333",
-            'issueDateTime': '111111',
-            'state': 'Reserved',
-            'student': 'Marcin Staszek',
-            'supervisor': 'promotor 3'
-        }
-    ];
+    theses: ThesisDto[] = null;
+    //[
+    //     {
+    //         'thesisNumber': 1,
+    //         'topic': "aaaa  vvv vvv",
+    //         'issueDateTime': '111111',
+    //         'state': 'free',
+    //         'student': 'brak',
+    //         'supervisor': 'promotor 1'
+    //     },
+    //     {
+    //         'thesisNumber': 2,
+    //         'topic': "aaaa  vvv vvv222222sssssssssssssssssssssssssssssssssss",
+    //         'issueDateTime': '111111',
+    //         'state': 'free',
+    //         'student': 'brak',
+    //         'supervisor': 'promotor 2'
+    //     },
+    //     {
+    //         'thesisNumber': 3,
+    //         'topic': "aaaa  vvv vvv3333333",
+    //         'issueDateTime': '111111',
+    //         'state': 'Reserved',
+    //         'student': 'Marcin Staszek',
+    //         'supervisor': 'promotor 3'
+    //     }
+    // ];
 
     constructor(
         private thesisService: ThesisService,
@@ -43,7 +45,7 @@ export class ThesesListComponent implements OnInit {
 
     ngOnInit(): void {
         this.thesisService.getTheses().subscribe((res) => {
-            // this.theses = res;
+            this.theses = res;
         });
     }
 
