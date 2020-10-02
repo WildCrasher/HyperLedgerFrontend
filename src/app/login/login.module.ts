4a94d602220ae9e8c72b943f112b7e45e55e8042
Marcin Staszak <m.staszak@wildasoftware.pl>
2020-10-02 17:13:31 +0200

    Login bug fixed.

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    RouterModule.forChild([
-      {
-        path: '',
-        component: LoginComponent,
-      }
-    ]),
-  ]
+    declarations: [],
+    imports: [
+        CommonModule,
+        RouterModule.forChild([
+            {
+                path: '',
+                component: LoginComponent,
+            }
+        ]),
+        FormsModule,
+        ReactiveFormsModule,
+    ]
 })
 export class LoginModule { }
