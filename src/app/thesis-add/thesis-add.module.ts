23b7b2dfc7e02de7854a9fd4a9bc5aa8fc971de5
Marcin Staszak <m.staszak@wildasoftware.pl>
2020-10-12 19:05:27 +0200

    Merge bug still bug.

--- src/app/thesis-add/thesis-add.module.ts
+++ src/app/thesis-add/thesis-add.module.ts
@@ -8,7 +8,6 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
     declarations: [ThesisAddComponent],
     imports: [
-        CommonModule,
         RouterModule.forChild([
             {
                 path: '',
