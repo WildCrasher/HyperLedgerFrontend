d4901751036b523670a6b1c96f19de65660ef345
Marcin Staszak <m.staszak@wildasoftware.pl>
2020-10-12 19:42:54 +0200

    Bug fixed...

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,9 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { InputMessageComponent } from '../shared/components/input-message/input-message.component';
 import { SharedModule } from '../shared/shared.module';
-
 
 @NgModule({
     declarations: [LoginComponent],
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InputMessageComponent } from './components/input-message/input-message.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
     declarations: [
         InputMessageComponent,
-        NavbarComponent,
-        FooterComponent,
     ],
     imports: [
         CommonModule
     ],
     exports: [
         InputMessageComponent,
-        NavbarComponent,
-        FooterComponent,
     ]
 })
 export class SharedModule { }
